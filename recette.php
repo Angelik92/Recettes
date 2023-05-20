57835d25d24d951b2c52bb1cbbedf6951f7eed01
Angelik92 <angelique.parizet@gmail.com>
2023-05-20 23:59:02 +0200

    Cours 7

--- recette.php
+++ recette.php
@@ -1,22 +1,21 @@
 <?php
 require_once('templates/header.php');
 require_once('lib/recipe.php');
+require_once('lib/tools.php');
 
-$pdo = new PDO('mysql:dbname=studi_live_cuisinea;host=localhost;charset=utf8mb4', 'root', '');
 
-$id = $_GET['id'];
+$id = (int)$_GET['id'];
 
-$query = $pdo->prepare("SELECT * FROM recipes WHERE id = :id");
-$query->bindParam(':id', $id, PDO::PARAM_INT);
-$query->execute();
-$recipe = $query->fetch();
+$recipe = getRecipeById($pdo, $id);
 
-// var_dump($recipe);
+if($recipe){
+    $ingredients = linesToArray($recipe['ingredients']);
+    $instructions = linesToArray($recipe['instructions']);
 
 ?>
 <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
     <div class="col-10 col-sm-8 col-lg-6">
-        <img src="<?=_RECIPES_IMG_PATH_.$recipe['image']?>" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" loading="lazy" width="700" height="500">
+        <img src="<?=getRecipeImage($recipe['image']);?>" class="d-block mx-lg-auto img-fluid" alt="<?= $recipe['title']?>" loading="lazy" width="700" height="500">
     </div>
     <div class="col-lg-6">
         <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3"><?= $recipe['title']?></h1>
@@ -24,7 +23,28 @@ $recipe = $query->fetch();
         
     </div>
 </div>
-            
+<div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+    <h2>Ingrédients</h2>
+    <ul class="list-group">
+        <?php foreach ($ingredients as $key => $ingredient) { ?>
+            <li class="list-group-item"><?=$ingredient; ?></li>
+        <?php } ?>
+    </ul>
+</div>
+
+<div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+    <h2>Instructions</h2>
+    <ol class="list-group list-group-numbered">
+        <?php foreach ($instructions as $key => $instruction) { ?>
+            <li class="list-group-item"><?=$instruction; ?></li>
+        <?php } ?>
+    </ol>
+</div>
+<?php } else { ?>
+    <div class="row text-center">
+        <h1> Recette introuvable</h1>
+    </div>
+<?php } ?>            
 <?php
     require_once('templates/footer.php');
 ?>
--- recettes.php
+++ recettes.php
@@ -1,11 +1,15 @@
 <?php
     require_once('templates/header.php');
-    require_once('lib/recipe.php')
+    require_once('lib/recipe.php');
+
+    $sql = 'SELECT * FROM recipes ORDER BY id DESC';
+
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    $recipes = $query->fetchAll();
 ?>
 
 <h1>Liste des recettes</h1>
-
-        <!-- Card -->
         <div class="row">
 
             <?php
